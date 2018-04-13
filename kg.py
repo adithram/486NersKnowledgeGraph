007d83ab06863d9d1b70ff18fcec021e2511931c
Adithya Ramanathan <adithram@umich.edu>
2018-04-13 12:02:42 -0400

    Complete knowledge graph querying

--- kg.py
+++ kg.py
@@ -8,26 +8,38 @@ import urllib
 # Read in missed topics from file
 queries = []
 with open('text_files/missed.txt', 'r') as f:
-	queries.append(f.readline())
+	queries = f.readlines()
+
+# strip new line characters
+queries = [x.strip() for x in queries] 
 
 
 api_key = open('.apikey').read()
-print api_key
 service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
 
 related_items = []
 
+
 for query in queries:
 	params = {
 	    'query': query,
-	    'limit': 3,
+	    'limit': 10,
 	    'indent': True,
 	    'key': api_key,
 	}
 	url = service_url + '?' + urllib.urlencode(params)
 	response = json.loads(urllib.urlopen(url).read())
 	related_items.append(response)
-for response in related_items:
-	print response
+
+output = ''
+for i, response in enumerate(related_items):
+	output += 'More information for ' + queries[i] + ' can be found by researching:\n'
+	output += '--------------------\n'
 	for element in response['itemListElement']:
-	  print (element['result']['name'] + ' (' + str(element['resultScore']) + ')')
+	  output +=  (element['result']['name']).encode('utf-8') + ' (' + str(element['resultScore']) + ')\n'
+
+	output += '\n'
+
+with open('text_files/suggested_topics.txt', 'w') as f:
+	f.write(output)
+
